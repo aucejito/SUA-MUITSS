3523c315424fb7a9bae9a59d10a4fa2a42b72f6a
Marcos Fernández Pérez <marferp2@inf.upv.es>
2022-05-22 11:28:27 +0200

    L3_HWCtoL3_TJC rule activation

--- selfadaptive.rules/src/selfadaptive/rules/Activator.java
+++ selfadaptive.rules/src/selfadaptive/rules/Activator.java
@@ -3,9 +3,13 @@ package selfadaptive.rules;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import selfadaptive.rules.ADS.L3_HWCtoL3_TJC;
+
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
+
+	private L3_HWCtoL3_TJC highwayChaufferToTrafficJam;
 
 	static BundleContext getContext() {
 		return context;
@@ -13,10 +17,17 @@ public class Activator implements BundleActivator {
 
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
+
+		highwayChaufferToTrafficJam = new L3_HWCtoL3_TJC(context);
+		context.addServiceListener(highwayChaufferToTrafficJam);
+
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
+
+		context.removeServiceListener(highwayChaufferToTrafficJam);
+		this.highwayChaufferToTrafficJam = null;
 	}
 
 }
