df79ea16b51e7f409c25abdd75aafbfe6358bad0
Marcos Fernández Pérez <marferp2@inf.upv.es>
2022-05-22 11:39:26 +0200

    Rules activator updated

--- selfadaptive.rules/src/selfadaptive/rules/Activator.java
+++ selfadaptive.rules/src/selfadaptive/rules/Activator.java
@@ -3,13 +3,21 @@ package selfadaptive.rules;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import selfadaptive.rules.ADS.L3_CCtoL3_HWC;
+import selfadaptive.rules.ADS.L3_CCtoL3_TJC;
 import selfadaptive.rules.ADS.L3_HWCtoL3_TJC;
+import selfadaptive.rules.ADS.L3_TJCtoL3_CC;
+import selfadaptive.rules.ADS.L3_TJCtoL3_HWC;
 
 public class Activator implements BundleActivator {
 
 	private static BundleContext context;
 
-	private L3_HWCtoL3_TJC highwayChaufferToTrafficJam;
+	private L3_CCtoL3_HWC cityToHighway;
+	private L3_CCtoL3_TJC cityToTrafficJam;
+	private L3_HWCtoL3_TJC highwayToTrafficJam;
+	private L3_TJCtoL3_CC trafficJamToCity;
+	private L3_TJCtoL3_HWC trafficJamToHighway;
 
 	static BundleContext getContext() {
 		return context;
@@ -18,16 +26,40 @@ public class Activator implements BundleActivator {
 	public void start(BundleContext bundleContext) throws Exception {
 		Activator.context = bundleContext;
 
-		highwayChaufferToTrafficJam = new L3_HWCtoL3_TJC(context);
-		context.addServiceListener(highwayChaufferToTrafficJam);
+		cityToHighway = new L3_CCtoL3_HWC(context);
+		context.addServiceListener(cityToHighway);
 
+		cityToTrafficJam = new L3_CCtoL3_TJC(context);
+		context.addServiceListener(cityToTrafficJam);
+
+		highwayToTrafficJam = new L3_HWCtoL3_TJC(context);
+		context.addServiceListener(highwayToTrafficJam);
+
+		trafficJamToCity = new L3_TJCtoL3_CC(context);
+		context.addServiceListener(trafficJamToCity);
+
+		trafficJamToHighway = new L3_TJCtoL3_HWC(context);
+		context.addServiceListener(trafficJamToHighway);
 	}
 
 	public void stop(BundleContext bundleContext) throws Exception {
 		Activator.context = null;
 
-		context.removeServiceListener(highwayChaufferToTrafficJam);
-		this.highwayChaufferToTrafficJam = null;
+		context.removeServiceListener(cityToHighway);
+		this.cityToHighway = null;
+
+		context.removeServiceListener(cityToTrafficJam);
+		this.cityToTrafficJam = null;
+
+		context.removeServiceListener(highwayToTrafficJam);
+		this.highwayToTrafficJam = null;
+
+		context.removeServiceListener(trafficJamToCity);
+		this.trafficJamToCity = null;
+
+		context.removeServiceListener(trafficJamToHighway);
+		this.trafficJamToHighway = null;
+
 	}
 
 }
