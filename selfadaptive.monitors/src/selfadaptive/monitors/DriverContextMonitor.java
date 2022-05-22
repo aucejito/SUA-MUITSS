a7de51abd9cbd2fd39fad02635bda8a13a51b448
Marcos Fernández Pérez <marferp2@inf.upv.es>
2022-05-22 17:03:16 +0200

    Typo corrections

--- selfadaptive.monitors/src/selfadaptive/monitors/DriverContextMonitor.java
+++ selfadaptive.monitors/src/selfadaptive/monitors/DriverContextMonitor.java
@@ -10,35 +10,35 @@ import sua.autonomouscar.interfaces.EFaceStatus;
 
 public class DriverContextMonitor {
 	protected BundleContext context;
-	
+
 	public DriverContextMonitor(BundleContext ctx) {
 		context = ctx;
 	}
-	
+
 	public void modifyHandsOnTheWheel(boolean newValue) {
 		HandsOnTheWheel handsOn = OSGiUtils.getService(context, HandsOnTheWheel.class);
 		boolean currentHandsOnTheWheel = handsOn.getHandsOnTheWheel();
-		if(currentHandsOnTheWheel != newValue && handsOn != null) {
+		if (currentHandsOnTheWheel != newValue && handsOn != null) {
 			handsOn.setHandsOnTheWheel(newValue);
-			System.out.print("Hands On The Wheel changed to " + newValue);
+			System.out.println("[Monitor - DriverContextMonitor] ----> Hands On The Wheel changed to " + newValue);
 		}
 	}
-	
+
 	public void modifyFaceStatus(EFaceStatus newFaceStatus) {
 		FaceStatus faceStatus = OSGiUtils.getService(context, FaceStatus.class);
 		EFaceStatus currentFaceStatus = faceStatus.getFaceStatus();
-		if(currentFaceStatus != newFaceStatus && faceStatus != null) {
+		if (currentFaceStatus != newFaceStatus && faceStatus != null) {
 			faceStatus.setFaceStatus(newFaceStatus);
-			System.out.print("Face Status changed to " + newFaceStatus);
+			System.out.println("[Monitor - DriverContextMonitor] ----> Face Status changed to " + newFaceStatus);
 		}
 	}
-	
+
 	public void modifyDriverSeatOcuppied(boolean newValue) {
 		DriverSeatOcuppied driverSeat = OSGiUtils.getService(context, DriverSeatOcuppied.class);
 		boolean currentDriverSeat = driverSeat.getDriverSeatOccupied();
-		if(currentDriverSeat != newValue && driverSeat != null) {
+		if (currentDriverSeat != newValue && driverSeat != null) {
 			driverSeat.setDriverSeatOcuppied(newValue);
-			System.out.print("Driver Seat Ocuppied changed to " + newValue);
+			System.out.println("[Monitor - DriverContextMonitor] ----> Driver Seat Ocuppied changed to " + newValue);
 		}
 	}
 }
